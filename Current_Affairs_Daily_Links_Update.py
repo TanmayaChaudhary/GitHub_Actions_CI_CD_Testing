082b75aed5da674f9bfe312fd29bad240621a12d
Tanmaya Chaudhary <40027119+TanmayaChaudhary@users.noreply.github.com>
2023-08-03 12:47:52 +0530

    Update Current_Affairs_Daily_Links_Update.py

--- Current_Affairs_Daily_Links_Update.py
+++ Current_Affairs_Daily_Links_Update.py
@@ -1,10 +1,13 @@
+import os
 import pandas as pd
 import requests
 import logging
 from datetime import datetime
 from bs4 import BeautifulSoup
 
+
 print("!! Script Run Start !!")
+
 # Configure logging
 logging.basicConfig(filename='web_scraping.log', level=logging.INFO,
                     format='%(asctime)s [%(levelname)s] %(message)s',
@@ -27,22 +30,26 @@ all_daily_updates_list = s.find_all('li')
 data = []
 
 # Load the existing Excel file (if any) into a DataFrame
+current_dir = os.path.dirname(os.path.abspath(__file__))
+print(f"current directory : {current_dir}")
+
+file_path = os.path.join(current_dir, 'daily_current_affairs_links1.csv')
+print(f"File Path : {file_path}")
+
 try:
-    existing_data = pd.read_csv('daily_current_affairs_links.csv')
+    existing_data = pd.read_csv(file_path)
 except FileNotFoundError:
     existing_data = pd.DataFrame(columns=["Date", "Link", "PDF Download Link"])
 
 # Iterate over the list and extract the date and link
 for li_element in all_daily_updates_list:
     try:
-
         href_url = li_element.find('a')['href']
         date = datetime.strptime(href_url.split('/')[-1], "%d-%m-%Y").strftime("%d %B %Y")
 
         print(f"Date : {date}, Link : {href_url}")
-
     except:
-        logging.error(f"href_url not present !")    
+        logging.error(f"href_url not present !")
 
     # Log the date
     logging.info(f"Date: {date}")
@@ -52,17 +59,13 @@ for li_element in all_daily_updates_list:
     soup = BeautifulSoup(response.text, 'html.parser')
 
     try:
-
         pdf_links = soup.find_all('a', class_='btn pdf')
-        print(pdf_links)
-        
         pdf_download_link = pdf_links[1].get('href')
         print(pdf_download_link)
     except:
         pdf_download_link = "Not available now!"
         logging.error(f"pdf download link not available for {href_url}!")
 
-    
     # Log the PDF download link
     logging.info(f"PDF Download Link: {pdf_download_link}")
 
@@ -73,7 +76,8 @@ for li_element in all_daily_updates_list:
 # Concatenate the data to the DataFrame
 combined_data = pd.concat([pd.DataFrame(data), existing_data])
 
-# Save the combined data to the Excel file
-combined_data.to_csv('daily_current_affairs_links.csv', index=False)
+# Save the combined data to the CSV file
+combined_data.to_csv(file_path, index=False)
 
 logging.info("Daily Current Affairs List Successfully Updated!")
+print("Daily Current Affairs List Successfully Updated!")
